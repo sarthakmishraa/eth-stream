d20b7c3b4cb9ac13a4cacb8e693b8a57ac8752d4
Sarthak Mishra <msarthak785@gmail.com>
2024-09-23 18:07:50 +0530

    auth fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,8 @@ import NextAuth from "next-auth";
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET ?? ""
+            clientId: process.env.NEXTAUTH_URL_GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.NEXTAUTH_URL_GOOGLE_CLIENT_SECRET ?? ""
         })
     ]
 });
